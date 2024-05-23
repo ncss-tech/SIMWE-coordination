8c6aff9884bc1cc6c5f9387b50a14f18e1d63bf6
Beaudette <dylan.beaudette@usda.gov>
2024-05-23 12:24:11 -0700

    updating tx069 example

--- sites/tx069-playas/get-DEM.R
+++ sites/tx069-playas/get-DEM.R
@@ -1,8 +1,7 @@
 library(elevatr)
-library(terra)
 library(sf)
 library(raster)
-
+library(terra)
 
 # https://casoilresource.lawr.ucdavis.edu/soil-properties/?prop=texture_025&lat=34.3774&lon=-101.7197&z=9
 # https://casoilresource.lawr.ucdavis.edu/gmap/?loc=34.47387,-102.12719,z13
@@ -11,19 +10,24 @@ bb <- '-102.1681 34.4483,-102.1681 34.5201,-102.0177 34.5201,-102.0177 34.4483,-
 wkt <- sprintf("POLYGON((%s))", bb)
 a <- vect(wkt, crs = 'epsg:4326')
 
-# for now use 10m data (z = 12)
+# for now use 8m data (z = 13)
 # later, use 4m data (z = 14)
 # have to work through sf / raster objects
-e <- get_elev_raster(st_as_sf(a), z = 12)
+e <- get_elev_raster(st_as_sf(a), z = 13)
 
 # convert to spatRaster
 e <- rast(e)
 
-# what is a reasonable local CRS?
+# unload raster package
+# conflicting namespace
+detach("package:raster", unload = TRUE)
 
+
+# reasonable local CRS: UTM z13 32613
 # consider warping in GRASS with r.proj
-
+# 
+e.utm <- terra::project(e, 'epsg:32613', method = 'cubicspline', res = 8)
 
 
 # save
-writeRaster(e, filename = 'grid/elev.tif', overwrite = TRUE, gdal = list('COMPRESS=LZW'))
+terra::writeRaster(e.utm, filename = 'grid/elev.tif', overwrite = TRUE, gdal = list('COMPRESS=LZW'))
